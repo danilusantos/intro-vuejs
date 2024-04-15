01f7fdb5eb69a289e5ba9301477b678b82eb123a
Danilo Santos <danilondosantos@gmail.com>
2024-04-15 17:25:11 -0300

    feat: trabalhando com o uso de v-for para estrutura de repetição

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const app = {
         return {
             name: 'Danilo',
             lastName: 'Nascimento dos Santos',
-            product: {
-                title: 'Série Sobrenatural',
-                description: 'Série Sobrenatural com 15 temporadas.',
-                image: './img/filme01.jpg',
-                stars: 5,
-            },
-            product2: {
-                title: 'Série Lúcifer',
-                description: 'Série Lúcifer com 6 temporadas.',
-                image: './img/filme02.jpg',
-                stars: 0
-            },
+            products: [
+                {
+                    title: 'Série Lúcifer',
+                    description: 'Série Lúcifer com 6 temporadas.',
+                    image: './img/filme02.jpg',
+                    stars: 3
+                },
+                {
+                    title: 'Série Sobrenatural',
+                    description: 'Série Sobrenatural com 15 temporadas.',
+                    image: './img/filme01.jpg',
+                    stars: 5,
+                }
+            ]
         }
     }
 }
