97c9c9c6ce7b5eabe1fdde1d952d1774f78a9a85
Danilo Santos <danilondosantos@gmail.com>
2024-04-15 17:17:18 -0300

    feat: trabalhando com o uso de renderização condicional

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const app = {
             product: {
                 title: 'Série Sobrenatural',
                 description: 'Série Sobrenatural com 15 temporadas.',
-                image: './img/filme01.jpg'
+                image: './img/filme01.jpg',
+                stars: 5,
             },
             product2: {
                 title: 'Série Lúcifer',
                 description: 'Série Lúcifer com 6 temporadas.',
-                image: './img/filme02.jpg'
-            }
+                image: './img/filme02.jpg',
+                stars: 0
+            },
         }
     }
 }
