05477309695a37d93e92cc21ebe7e838760d1f55
Danilo Santos <danilondosantos@gmail.com>
2024-04-15 17:03:49 -0300

    feat: aprendendo o uso de bind do vuejs

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const app = {
         return {
             name: 'Danilo',
             lastName: 'Nascimento dos Santos',
+            product: {
+                title: 'Série Sobrenatural',
+                description: 'Série Sobrenatural com 15 temporadas.',
+                image: './img/filme01.jpg'
+            },
+            product2: {
+                title: 'Série Lúcifer',
+                description: 'Série Lúcifer com 6 temporadas.',
+                image: './img/filme02.jpg'
+            }
         }
     }
 }
